df17fed7353bedb5935c8648f900ede1867b2a88
Damian Szczepanik <damian.szczepanik@sollers.pl>
2015-03-10 22:13:47 +0100

    target directory was dropped twice: via rm and mvn clean

--- checkstyle-tester/launch.sh
+++ checkstyle-tester/launch.sh
@@ -14,29 +14,22 @@ cat projects-to-test-on.properties | while read line; do
     
     if [ -d "$REPO_SOURCES_DIR" ]; then
       echo "Pulling repository '$REPO_NAME' ..."
-	  cd $REPO_SOURCES_DIR
-	  git pull
-	  cd -
-	  echo -e "Pulling repository '$REPO_NAME' - successful\n"
-	else
-	  echo "Cloning repository '$REPO_NAME' ..."
+      cd $REPO_SOURCES_DIR
+      git pull
+      cd -
+      echo -e "Pulling repository '$REPO_NAME' - completed\n"
+    else
+      echo "Cloning repository '$REPO_NAME' ..."
       git clone $REPO_URL $REPO_SOURCES_DIR
-      echo -e "Cloning repository '$REPO_NAME' - successful\n"
-	fi
+      echo -e "Cloning repository '$REPO_NAME' - completed\n"
+    fi
 
 done
-
-if [ -d "target" ]; then
-  echo "Existing target directory will be cleared"
-  rm -rf target/*
-else
-  mkdir "target"
-fi
 
 mvn --batch-mode clean
 
 echo "Running Checkstyle on $SOURCES_DIR ..."
-time mvn --batch-mode site "$@" # > target/console_log.txt
+mvn --batch-mode site "$@"
 echo "Running Checkstyle on $SOURCES_DIR - finished"
 
 echo "Testing Checkstyle finished"
