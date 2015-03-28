31819387e4093135d34e9055f2eb3b651ac6e50b
Roman Ivanov <ivanov-jr@mail.ru>
2015-03-28 08:12:49 -0700

    release notes builder , draft version

---
+++ releasenotes-builder/src/NotesBuilder.groovy
@@ -0,0 +1,55 @@
+
+class NotesBuilder {
+
+    static main(args) {
+       def path = "/home/rivanov/workspace-checkstyle/NotesBuilder/src/input.txt"
+       if (args.length != 0) {
+           path = args[0] 
+       }
+       
+       doNotes(path)
+    }
+
+    static void doNotes(String filePath) {
+        def f = new File(filePath)
+        f.eachLine() { line ->
+            //println "LINE:"+line
+
+                        
+            def index = line.indexOf(". Author")
+            def author = line.substring(index + 8 + 1, line.size())
+          
+            def message = line.substring(0, index)
+            def issueIndex = message.indexOf(", issue")
+            def messageContent = message
+            if (issueIndex != -1) {
+                messageContent = message.substring(0, issueIndex)
+            }
+            
+            def issue = ""
+            if (issueIndex != -1) {
+               issue = message.substring(message.lastIndexOf(',')+2, message.size())
+               issue = issue.split("#")[1]
+            }
+            
+//            if (issue == "") {
+//                println "PARSE ERROR: (no issue), LINE:" + line;
+//                System.exit(1)
+//            }
+            
+            if (issue != "") {
+            print """
+        <li>
+          $messageContent. Author: $author <a href="https://github.com/checkstyle/checkstyle/issues/$issue">#$issue</a>
+        </li>""" 
+            } else {
+                print """
+        <li>
+          $messageContent. Author: $author
+        </li>""" 
+            
+            }
+        
+        }
+    }
+}
