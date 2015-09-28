aee4215c3812854961d50fb90fb62077bb31f6df
Roman Ivanov <ivanov-jr@mail.ru>
2015-09-27 17:57:55 -0700

    format that enforces by UTs is now supported by generator

--- releasenotes-builder/src/NotesBuilder.groovy
+++ releasenotes-builder/src/NotesBuilder.groovy
@@ -4,9 +4,9 @@ class NotesBuilder {
     static main(args) {
        def path = "input.txt"
        if (args.length != 0) {
-           path = args[0] 
+           path = args[0]
        }
-       
+
        doNotes(path)
     }
 
@@ -14,51 +14,53 @@ class NotesBuilder {
         def f = new File(filePath)
         f.eachLine() { line ->
             //println "LINE:"+line
-            if (line == "")
+
+            if (line == "") {
+              // keep separators from source
+              print "\n"
               return // continue
-                        
-            def index = line.indexOf(". Author")
-            if (index == -1)
-              return // continue as it some weird line
-
-            def author = line.substring(index + 8 + 1, line.size())
-          
-            def message = line.substring(0, index)
-            if (message =~ ("[,.] [iI]ssue #")) {
-                throw new RuntimeException("old format was detected, expeted format '. #XXXXX.', actual message: $message");
             }
 
-            def issueIndex = message.indexOf(". #")
-            def messageContent = message
-            if (issueIndex != -1) {
-                messageContent = message.substring(0, issueIndex)
+            def indexIssue = line.indexOf("Issue #")
+            def indexPull = line.indexOf("Pull #")
+            def indexPrefix = line.indexOf(":")
+            if (indexIssue != 0 && indexPull != 0 && indexPrefix > 5) {
+              throw new RuntimeException("strange format was detected at line: $line");
             }
-            
+
             def issue = ""
-            if (issueIndex != -1) {
-               issue = message.substring(message.lastIndexOf(',')+2, message.size())
-               issue = issue.split("#")[1]
-               issue = issue.trim();
-            }
-            
-//            if (issue == "") {
-//                println "PARSE ERROR: (no issue), LINE:" + line;
-//                System.exit(1)
-//            }
-            
-            if (issue != "") {
-            print """
-        <li>
-          $messageContent. Author: $author <a href="https://github.com/checkstyle/checkstyle/issues/$issue">#$issue</a>
-        </li>""" 
+            def indexMessage = line.indexOf(": ");
+            if (indexIssue == 0) {
+              // this is Issue
+              issue = line.substring(7, indexMessage)
+            } else if (indexPull == 0) {
+              // this is Pull
+              issue = line.substring(6, indexMessage)
             } else {
+              // this is some minor
+              issue = ""
+            }
+
+            def message = line.substring(indexMessage + 2, line.size())
+
+            if (issue != "" && indexIssue == 0) {
+              print """
+        <li>
+          $message <a href="https://github.com/checkstyle/checkstyle/issues/$issue">#$issue</a>
+        </li>"""
+            } else if (issue != "" && indexPull == 0) {
+              print """
+        <li>
+          $message <a href="https://github.com/checkstyle/checkstyle/pull/$issue">#$issue</a>
+        </li>"""
+            }else {
                 print """
         <li>
-          $messageContent. Author: $author
-        </li>""" 
-            
+          $message
+        </li>"""
+
             }
-        
+
         }
         print """\n"""
     }
