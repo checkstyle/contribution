fdb6d073d167b5f0ec8eed85eac63b292f3eb2bf
rnveach <rveach02@gmail.com>
2017-05-20 12:08:54 -0400

    launch_diff: upgraded util to have same features as groovy scripts

---
+++ checkstyle-tester/launch_diff_variables.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ============================================================
+# Custom Options
+# Note: Use full paths
+# ============================================================
+
+MINIMIZE=true
+CONTACTSERVER=true
+
+PULL_REMOTE=pull
+
+CHECKSTYLE_DIR=~/checkstyle
+SEVNTU_DIR=~/sevntu.checkstyle
+CONTRIBUTION_DIR=~/contribution
+
+TESTER_DIR=$CONTRIBUTION_DIR/checkstyle-tester
+DIFF_JAR=$CONTRIBUTION_DIR/patch-diff-report-tool/target/patch-diff-report-tool-0.1-SNAPSHOT-jar-with-dependencies.jar
+
+REPOSITORIES_DIR=$TESTER_DIR/repositories
+FINAL_RESULTS_DIR=$TESTER_DIR/reports/diff
+
+SITE_SOURCES_DIR=$TESTER_DIR/src/main/java
+SITE_SAVE_MASTER_DIR=$TESTER_DIR/reports/savemaster
+SITE_SAVE_PULL_DIR=$TESTER_DIR/reports/savepull
+SITE_SAVE_REF_DIR=$TESTER_DIR/reports/saverefs
+
+# ============================================================
+# ============================================================
+# ============================================================
