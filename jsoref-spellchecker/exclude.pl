fff31afe17644b9e4bf32f85b92c5d16afff59c3
Josh Soref <jsoref@users.noreply.github.com>
2018-04-15 02:51:24 +0000

    minor: migrate exclusion code from checkstyle for checkstyle/checkstyle#5719

---
+++ jsoref-spellchecker/exclude.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+# This script takes null delimited files as input
+# it drops paths that match the listed exclusions
+# output is null delimited to match input
+$/="\0";
+my @excludes=qw(
+  /images/
+  /src/it/resources/
+  /src/test/resources/
+  /messages.*_..\.properties$
+  /.*_..\.translation[^/]*$
+);
+my $exclude = join "|", @excludes;
+while (<>) {
+  chomp;
+  next if m{$exclude};
+  print "$_$/";
+}
