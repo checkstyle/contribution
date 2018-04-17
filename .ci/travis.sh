7ed314d78eeccea742065aed689a82e452b61bc7
Roman Ivanov <ivanov-jr@mail.ru>
2018-04-17 15:48:16 -0700

    monor: make multiline jsoref-spellchecker in travis.sh

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -5,7 +5,9 @@ set -e
 case $1 in
 
 jsoref-spellchecker)
-  git clone https://github.com/checkstyle/checkstyle && cd checkstyle && mkdir -p .ci-temp/contribution
+  git clone https://github.com/checkstyle/checkstyle
+  cd checkstyle
+  mkdir -p .ci-temp/contribution
   cp -r ../jsoref-spellchecker .ci-temp/contribution
   export skipFetchRepo=true && ./.ci/test-spelling-unknown-words.sh
   ;;
