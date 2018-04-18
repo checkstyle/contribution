2239734c99e6244943e1e5ebfb0e88baf5be5291
Roman Ivanov <ivanov-jr@mail.ru>
2018-04-18 14:06:33 -0700

    minor: remove spell checker as it is moved to main repo checkstyle/checkstyle#5740

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -4,12 +4,7 @@ set -e
 
 case $1 in
 
-jsoref-spellchecker)
-  git clone https://github.com/checkstyle/checkstyle
-  cd checkstyle
-  mkdir -p .ci-temp/contribution
-  cp -r ../jsoref-spellchecker .ci-temp/contribution
-  export skipFetchRepo=true && ./.ci/test-spelling-unknown-words.sh
+some-goal)
   ;;
 
 *)
