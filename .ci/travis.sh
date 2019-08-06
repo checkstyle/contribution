3c9b1fab4b108db0c318f1fe78a59b7ca2700689
Roman Ivanov <ivanov-jr@mail.ru>
2019-08-06 11:27:42 -0700

    config: move all travis commands to travis.sh

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -4,7 +4,56 @@ set -e
 
 case $1 in
 
-some-goal)
+releasenotes-builder)
+  cd releasenotes-builder
+  mvn clean verify
+  mvn clean compile package
+  ;;
+
+patch-diff-report-tool)
+  cd patch-diff-report-tool
+  mvn clean install
+  ;;
+
+checkstyle-tester-launch-groovy)
+  git clone https://github.com/checkstyle/checkstyle
+  cd checkstyle
+  mvn -B clean install -Passembly
+  cd ../checkstyle-tester
+  groovy launch.groovy -l projects-for-travis.properties -c my_check.xml -i
+  ;;
+
+checkstyle-tester-diff-groovy-patch)
+  git clone https://github.com/checkstyle/checkstyle
+  cd checkstyle
+  git checkout -b patch-branch
+  cd ../checkstyle-tester
+  groovy diff.groovy -l projects-for-travis.properties \
+    -c my_check.xml -b master -p patch-branch -r ../checkstyle
+  ;;
+
+checkstyle-tester-diff-groovy-base-patch)
+  git clone https://github.com/checkstyle/checkstyle
+  cd checkstyle
+  git checkout -b patch-branch
+  cd ../checkstyle-tester
+  groovy diff.groovy -l projects-for-travis.properties \
+    -bc my_check.xml -pc my_check.xml -b master -p patch-branch -r ../checkstyle
+  ;;
+
+checkstyle-tester-diff-groovy-patch-only)
+  git clone https://github.com/checkstyle/checkstyle
+  cd checkstyle
+  git checkout -b patch-branch
+  cd ../checkstyle-tester
+  groovy diff.groovy -l projects-for-travis.properties \
+    -pc my_check.xml -p patch-branch -r ../checkstyle -m single
+  ;;
+
+codenarc)
+  cd checkstyle-tester
+  ./codenarc.sh . diff.groovy > diff.log && cat diff.log && grep '(p1=0; p2=8; p3=0)' diff.log
+  ./codenarc.sh . launch.groovy > launch.log && cat launch.log && grep '(p1=0; p2=21; p3=1)' launch.log
   ;;
 
 *)
