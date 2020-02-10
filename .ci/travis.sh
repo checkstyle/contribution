34c185d8e5f0abac69a1077419b97fa8cfec7a92
Roman Ivanov <ivanov-jr@mail.ru>
2020-02-09 18:03:23 -0800

    config: checkout all .ci-temp folder

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -2,6 +2,22 @@
 # Attention, there is no "-x" to avoid problems on Travis
 set -e
 
+function checkout_from {
+  CLONE_URL=$1
+  PROJECT=$(echo "$CLONE_URL" | sed -nE 's/.*\/(.*).git/\1/p')
+  mkdir -p .ci-temp
+  cd .ci-temp
+  if [ -d "$PROJECT" ]; then
+    echo "Target project $PROJECT is already cloned, latest changes will be fetched"
+    cd $PROJECT
+    git fetch
+    cd ../
+  else
+    for i in 1 2 3 4 5; do git clone $CLONE_URL && break || sleep 15; done
+  fi
+  cd ../
+}
+
 case $1 in
 
 releasenotes-builder)
@@ -16,38 +32,40 @@ patch-diff-report-tool)
   ;;
 
 checkstyle-tester-launch-groovy)
-  git clone https://github.com/checkstyle/checkstyle
-  cd checkstyle
+  checkout_from https://github.com/checkstyle/checkstyle
+  cd .ci-temp/checkstyle
   mvn --batch-mode clean install -Passembly
-  cd ../checkstyle-tester
+  cd ../../checkstyle-tester
   groovy launch.groovy -l projects-for-travis.properties -c my_check.xml -i
   ;;
 
 checkstyle-tester-diff-groovy-patch)
-  git clone https://github.com/checkstyle/checkstyle
-  cd checkstyle
+  checkout_from https://github.com/checkstyle/checkstyle
+  cd .ci-temp/checkstyle
   git checkout -b patch-branch
-  cd ../checkstyle-tester
+  cd ../../checkstyle-tester
+  #sed -i'' 's/^guava/#guava/' projects-for-wercker.properties
+  #sed -i'' 's/#checkstyle/checkstyle/' projects-for-wercker.properties
   groovy diff.groovy -l projects-for-travis.properties \
-    -c my_check.xml -b master -p patch-branch -r ../checkstyle
+    -c my_check.xml -b master -p patch-branch -r ../.ci-temp/checkstyle
   ;;
 
 checkstyle-tester-diff-groovy-base-patch)
-  git clone https://github.com/checkstyle/checkstyle
-  cd checkstyle
+  checkout_from https://github.com/checkstyle/checkstyle
+  cd .ci-temp/checkstyle
   git checkout -b patch-branch
-  cd ../checkstyle-tester
+  cd ../../checkstyle-tester
   groovy diff.groovy -l projects-for-travis.properties \
-    -bc my_check.xml -pc my_check.xml -b master -p patch-branch -r ../checkstyle
+    -bc my_check.xml -pc my_check.xml -b master -p patch-branch -r ../.ci-temp/checkstyle
   ;;
 
 checkstyle-tester-diff-groovy-patch-only)
-  git clone https://github.com/checkstyle/checkstyle
-  cd checkstyle
+  checkout_from https://github.com/checkstyle/checkstyle
+  cd .ci-temp/checkstyle
   git checkout -b patch-branch
-  cd ../checkstyle-tester
+  cd ../../checkstyle-tester
   groovy diff.groovy -l projects-for-travis.properties \
-    -pc my_check.xml -p patch-branch -r ../checkstyle -m single
+    -pc my_check.xml -p patch-branch -r ../.ci-temp/checkstyle -m single
   ;;
 
 codenarc)
