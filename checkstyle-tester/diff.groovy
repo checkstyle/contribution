7b89c87f2375380192a645f38c2edbe39154f75a
rnveach <rveach02@gmail.com>
2020-04-26 10:53:25 -0400

    add error print to diff.groovy

--- checkstyle-tester/diff.groovy
+++ checkstyle-tester/diff.groovy
@@ -183,7 +183,7 @@ def generateCheckstyleReport(cfg) {
 
     def checkstyleVersion = getCheckstyleVersionFromPomXml("$cfg.localGitRepo/pom.xml", 'version')
 
-    executeCmd("mvn --batch-mode -Pno-validations clean install", cfg.localGitRepo)
+    executeCmd("mvn -e --batch-mode -Pno-validations clean install", cfg.localGitRepo)
     executeCmd("""groovy launch.groovy --listOfProjects $cfg.listOfProjects
             --config $cfg.checkstyleCfg --ignoreExceptions --ignoreExcludes --checkstyleVersion $checkstyleVersion""")
     println "Moving Checkstyle report into $cfg.destDir ..."
@@ -211,7 +211,7 @@ def generateDiffReport(cfg) {
         .parent
         .resolve("patch-diff-report-tool")
         .toFile()
-    executeCmd("mvn --batch-mode clean package -DskipTests", diffToolDir)
+    executeCmd("mvn -e --batch-mode clean package -DskipTests", diffToolDir)
     def diffToolJarPath = getPathToDiffToolJar(diffToolDir)
 
     println 'Starting diff report generation ...'
