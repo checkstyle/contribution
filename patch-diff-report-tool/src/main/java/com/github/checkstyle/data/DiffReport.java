d0cf2438be6eaeb3fac7744171092f868ebc9fce
Pavel Bludov <pbludov@yandex-team.ru>
2020-12-27 13:56:16 +0300

    Issue #532: optimize DiffReport

--- patch-diff-report-tool/src/main/java/com/github/checkstyle/data/DiffReport.java
+++ patch-diff-report-tool/src/main/java/com/github/checkstyle/data/DiffReport.java
@@ -19,9 +19,7 @@
 
 package com.github.checkstyle.data;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -76,16 +74,16 @@ public final class DiffReport {
     public void addRecords(List<CheckstyleRecord> newRecords,
             String filename) {
         if (!newRecords.isEmpty()) {
+            Collections.sort(newRecords);
             final List<CheckstyleRecord> popped =
                 records.put(filename, newRecords);
             if (popped != null) {
                 final List<CheckstyleRecord> diff =
-                    produceDiff(popped, newRecords);
+                    DiffUtils.produceDiff(popped, newRecords);
                 if (diff.isEmpty()) {
                     records.remove(filename);
                 }
                 else {
-                    Collections.sort(diff, new PositionOrderComparator());
                     records.put(filename, diff);
                 }
             }
@@ -93,94 +91,30 @@ public final class DiffReport {
     }
 
     /**
-     * Creates difference between 2 lists of records.
-     *
-     * @param list1
-     *        the first list.
-     * @param list2
-     *        the second list.
-     * @return the difference list.
-     */
-    private static List<CheckstyleRecord> produceDiff(
-            List<CheckstyleRecord> list1, List<CheckstyleRecord> list2) {
-        final List<CheckstyleRecord> diff = new ArrayList<>();
-        for (CheckstyleRecord rec1 : list1) {
-            if (!isInList(list2, rec1)) {
-                diff.add(rec1);
-            }
-        }
-        for (CheckstyleRecord rec2 : list2) {
-            if (!isInList(list1, rec2)) {
-                diff.add(rec2);
-            }
-        }
-        return diff;
-    }
-
-    /**
-     * Compares the record against list of records.
-     *
-     * @param list
-     *        of records.
-     * @param testedRecord
-     *        the record.
-     * @return true, if has its copy in a list.
-     */
-    private static boolean isInList(List<CheckstyleRecord> list,
-            CheckstyleRecord testedRecord) {
-        boolean belongsToList = false;
-        for (CheckstyleRecord checkstyleRecord : list) {
-            if (testedRecord.specificEquals(checkstyleRecord)) {
-                belongsToList = true;
-                break;
-            }
-        }
-        return belongsToList;
-    }
-
-    /**
      * Generates statistical information and puts in in the accumulator.
+     * This method will wait for completion of all asynchronous tasks.
      */
     public void getDiffStatistics() {
         statistics.setFileNumDiff(records.size());
-        for (Map.Entry<String, List<CheckstyleRecord>> entry
-                : records.entrySet()) {
-            final List<CheckstyleRecord> list = entry.getValue();
-            for (CheckstyleRecord rec : list) {
-                if (rec.getIndex() == CheckstyleReportsParser.BASE_REPORT_INDEX) {
-                    statistics.addSeverityRecordRemoved(rec.getSeverity());
-                    statistics.addModuleRecordRemoved(rec.getSource());
-                }
-                else {
-                    statistics.addSeverityRecordAdded(rec.getSeverity());
-                    statistics.addModuleRecordAdded(rec.getSource());
-                }
-                statistics.incrementUniqueMessageCount(rec.getIndex());
-            }
-        }
+        records.entrySet().stream()
+            .flatMap(entry -> entry.getValue().stream())
+            .forEach(this::addRecordStatistics);
     }
 
     /**
-     * Comparator used to sort lists of CheckstyleRecord objects
-     * by their position in code.
+     * Generates statistical information for one CheckstyleRecord.
      *
-     * @author atta_troll
-     *
+     * @param checkstyleRecord the checkstyleRecord to process
      */
-    private static class PositionOrderComparator
-        implements Comparator<CheckstyleRecord> {
-
-        @Override
-        public int compare(final CheckstyleRecord arg0,
-                final CheckstyleRecord arg1) {
-            final int difference = arg0.getLine() - arg1.getLine();
-            if (difference == 0) {
-                return arg0.getColumn() - arg1.getColumn();
-            }
-            else {
-                return difference;
-            }
+    private void addRecordStatistics(CheckstyleRecord checkstyleRecord) {
+        if (checkstyleRecord.getIndex() == CheckstyleReportsParser.BASE_REPORT_INDEX) {
+            statistics.addSeverityRecordRemoved(checkstyleRecord.getSeverity());
+            statistics.addModuleRecordRemoved(checkstyleRecord.getSource());
         }
+        else {
+            statistics.addSeverityRecordAdded(checkstyleRecord.getSeverity());
+            statistics.addModuleRecordAdded(checkstyleRecord.getSource());
+        }
+        statistics.incrementUniqueMessageCount(checkstyleRecord.getIndex());
     }
-
 }
