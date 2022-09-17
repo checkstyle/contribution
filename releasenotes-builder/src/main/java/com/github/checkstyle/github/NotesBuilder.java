21ad13659f978fc622f9d7c5f288b639a024ee30
rnveach <rveach02@gmail.com>
2022-09-16 20:12:00 -0400

    infra: upgrade dependency versions for releasenotes-builder

--- releasenotes-builder/src/main/java/com/github/checkstyle/github/NotesBuilder.java
+++ releasenotes-builder/src/main/java/com/github/checkstyle/github/NotesBuilder.java
@@ -339,9 +339,8 @@ public final class NotesBuilder {
      *
      * @param issue issue.
      * @return issue label for release notes
-     * @throws IOException if an I/o error occurs.
      */
-    private static String getIssueLabelFrom(GHIssue issue) throws IOException {
+    private static String getIssueLabelFrom(GHIssue issue) {
         final Collection<GHLabel> issueLabels = issue.getLabels();
         final Optional<GHLabel> label = issueLabels.stream()
             .filter(input -> Arrays.binarySearch(Constants.ISSUE_LABELS, input.getName()) >= 0)
@@ -354,9 +353,8 @@ public final class NotesBuilder {
      *
      * @param issue issue.
      * @return release label for release notes
-     * @throws IOException if an I/o error occurs.
      */
-    private static List<GHLabel> getAllIssueLabels(GHIssue issue) throws IOException {
+    private static List<GHLabel> getAllIssueLabels(GHIssue issue) {
         final Collection<GHLabel> issueLabels = issue.getLabels();
         return issueLabels.stream()
             .filter(input -> Arrays.binarySearch(Constants.ISSUE_LABELS, input.getName()) >= 0)
