0abb363a76429b4c295576f77f4982432587cb1b
Amit Kumar Deohoria <kamit5741@gmail.com>
2025-10-19 17:44:39 +0530

    Issue #980: Refactoring AntBuilder movedir to use Built in groovy ops

--- checkstyle-tester/diff.groovy
+++ checkstyle-tester/diff.groovy
@@ -717,9 +717,8 @@ def copyDir(source, destination) {
 }
 
 def moveDir(source, destination) {
-    new AntBuilder().move(todir: destination) {
-        fileset(dir: source)
-    }
+    copyDir(source, destination)
+    deleteDir(source)
 }
 
 def deleteDir(dir) {
