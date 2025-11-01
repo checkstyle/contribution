f787159db771d4d9cb6f8b2b83ac4e45178ed524
Roman Ivanov <romani@users.noreply.github.com>
2025-10-31 18:13:08 -0700

    Revert "Issue 980: Refactor AntBuilder CopyDir to use Built in Groovy ops"
    This reverts commit dcb5e381d2be42d642d0620c47f7cb80e4fe9244.

--- checkstyle-tester/diff.groovy
+++ checkstyle-tester/diff.groovy
@@ -713,33 +713,8 @@ def postProcessCheckstyleReport(targetDir, repoName, repoPath) {
 }
 
 def copyDir(source, destination) {
-    def sourceDir = new File(source)
-    def destDir = new File(destination)
-
-    if (!sourceDir.exists() || !sourceDir.isDirectory()) {
-        throw new IllegalArgumentException("Source directory does not exist or is not a directory: $source")
-    }
-
-    if (!destDir.exists()) {
-        if (!destDir.mkdirs()) {
-            throw new IOException("Failed to create destination directory: $destination")
-        }
-    }
-
-    sourceDir.eachFileRecurse { file ->
-        def relativePath = sourceDir.toPath().relativize(file.toPath()).toString()
-        def destFile = new File(destDir, relativePath)
-
-        if (file.isDirectory()) {
-            if (!destFile.exists()) {
-                if (!destFile.mkdirs()) {
-                    throw new IOException("Failed to create directory: ${destFile.path}")
-                }
-            }
-        } else {
-            destFile.parentFile.mkdirs()
-            Files.copy(file.toPath(), destFile.toPath(), REPLACE_EXISTING)
-        }
+    new AntBuilder().copy(todir: destination) {
+        fileset(dir: source)
     }
 }
 
